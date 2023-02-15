9d6809b79b7c1640a14c90c423385666fd236042
Karolis Rakauskas <Karolis.Rakausku@gmail.com>
2023-02-16 01:01:42 +0200

    Added EmailSender

--- DrawingRegisterWeb/Program.cs
+++ DrawingRegisterWeb/Program.cs
@@ -2,18 +2,26 @@ using DrawingRegisterWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using DrawingRegisterWeb.Models;
+using DrawingRegisterWeb.Utilities;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Add Database context
 builder.Services.AddDbContext<DrawingRegisterContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+// Add Default Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
 	.AddRoles<IdentityRole>().AddEntityFrameworkStores<DrawingRegisterContext>();
+
+// Configure app to support email
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
+// Seed Data : TODO Seed Database Migrations Automatically
 using (var scope = app.Services.CreateScope())
 {
 	var services = scope.ServiceProvider;
